baf75f63971b113d35c539c53e4af1e5635063ad
insideoutzombie <david@web3devs.com>
2018-08-20 15:02:40 -0500

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 // GET method is for reading the data from the above app.post method.
 
 app.listen(3000, () => {
